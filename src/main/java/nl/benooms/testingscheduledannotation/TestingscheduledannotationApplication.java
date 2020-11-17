f820fe2d0bf4ead60a8fb50915cdadd90521e966
Ben Ooms <benoomsdh@gmail.com>
2020-11-17 20:21:40 +0100

    Generated project from start.spring.io

---
+++ src/main/java/nl/benooms/testingscheduledannotation/TestingscheduledannotationApplication.java
@@ -0,0 +1,13 @@
+package nl.benooms.testingscheduledannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingscheduledannotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingscheduledannotationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/benooms/testingscheduledannotation/TestingscheduledannotationApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.benooms.testingscheduledannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingscheduledannotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
